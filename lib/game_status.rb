11be563577c77d336f6bd51a1816dc15f8b24fbf
Alena Trushnikova <alena.yu.t@gmail.com>
2020-09-11 23:54:21 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -6,18 +6,19 @@ end
 # Define your WIN_COMBINATIONS constant
 
 WIN_COMBINATIONS = [
-  [0,1,2],
-  [3,4,5],
-  [6,7,8],
-  [0,3,6],
-  [2,5,8],
-  [1,4,7],
-  [0,4,8],
-  [6,4,2]
+  [0,1,2], #1
+  [3,4,5], #2
+  [6,7,8], #3
+  [0,3,6], #4
+  [2,5,8], #5
+  [1,4,7], #6
+  [0,4,8], #7
+  [6,4,2]  #8
 ]
 
 def won?(board)
   WIN_COMBINATIONS.each {|win_combination|
   win_index_1 = win_combination[0]
-  
+  win_index_2 = win_combination[1]
+  win_index_3 = win_combination[2]
 end
