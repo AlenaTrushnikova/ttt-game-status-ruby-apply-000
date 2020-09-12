23b687a1b1ac1e5d4f4f68aae9454bd7621af2ae
Alena Trushnikova <alena.yu.t@gmail.com>
2020-09-12 00:10:46 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -38,3 +38,11 @@ end
 def full?(board)
   board.all?{|token| token == "X" || token == "O"}
 end
+
+def draw?(board)
+  if !won?(board) && full?(board)
+    return true
+  else
+    return false
+  end
+end
